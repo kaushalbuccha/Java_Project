0adeea6e5500c2a44b8dcd1140e707dd161e7eee
hemant2745 <95204778+hemant2745@users.noreply.github.com>
2023-04-15 09:39:01 +0530

    Update BusManagementSystem.java

--- BusManagementSystem.java
+++ BusManagementSystem.java
@@ -117,6 +117,125 @@ class Operations{
 }
 public class BusManagementSystem {
     
+ import java.util.*;
+class Passenger{
+    String name;
+    int age;
+    long con;
+    String start;
+    String end;
+    int seat;
+
+    Passenger(String s,int a,long c,String st,String e){
+        name = s;
+        age = a;
+        con = c;
+        start = st;
+        end = e;
+        seat = x;
+    }
+}
+
+class Operations{
+	ArrayList<Passenger> arr = new ArrayList<>();
+
+    int vacantSeat(){
+        if(arr.size() == 0){
+            return 0;
+        }
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.get(i).seat != (i+1))
+                return i;
+        }
+        return -1;
+    }
+    void booking(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter name");
+        String s = sc.nextLine();
+        System.out.println("Enter age");
+        int a = sc.nextInt();
+        System.out.println("Enter contact number");
+        long c = sc.nextLong();
+        sc.nextLine();
+        System.out.println("Enter starting point");
+        String st = sc.next();
+        System.out.println("Enter destination");
+        String e = sc.next();
+        int x = vacantSeat(); // This functions returns the vacant seat in case of seat is vacant by cancelling
+
+        if(x == -1)
+            x = arr.size();
+        if(arr.size() < 40){
+            arr.add(x,new Passenger(s , a , c , st , e, (x+1)));
+		}
+        else
+            System.out.println("No more seats available");
+
+        System.out.println();
+        System.out.println("|-------------------|");
+        System.out.println("    Ticket Booked ");
+        System.out.println("    Seat number : "+(x+1));
+        System.out.println("|-------------------|");
+        System.out.println();
+    }
+
+    void cancel(){
+        Scanner sc = new Scanner(System.in);
+        if(arr.size() >= 1){
+            System.out.println("Enter seat number");
+            int st = sc.nextInt();
+            for (int i = 0; i < arr.size();i++) {
+                if(arr.get(i).seat == st){
+                    arr.remove(i);
+                }
+            }
+            System.out.println();
+            System.out.println("|-------------------|");
+            System.out.println("   Ticket Cancelled ");
+            System.out.println("|-------------------|");
+            System.out.println();
+        }
+        else{
+            System.out.println();
+            System.out.println("No passenger available");
+            System.out.println();
+        }
+        
+    }
+
+    void seatAvailblity(){
+        System.out.println();
+        System.out.println("Available seats: "+ (40-arr.size()));
+        System.out.println();
+    }
+
+    void passengerDetail(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter seat number");
+        int st = sc.nextInt();
+        for (Passenger p : arr) {
+            if(p.seat == st){
+				System.out.println();
+                System.out.println("Passeger Details:");
+                System.out.println("Name : "+p.name);
+                System.out.println("Age : "+p.age);
+                System.out.println("Mobile No. : "+p.con);
+                System.out.println("Boarding : "+p.start);
+                System.out.println("Destination : "+p.end);
+				System.out.println();
+				System.out.println();
+                return;
+            }
+        }
+        System.out.println();
+        System.out.println("Seat "+st+" is vacant");
+        System.out.println();
+    }
+}
+public class BusManagementSystem {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome");
@@ -142,5 +261,7 @@ public class BusManagementSystem {
             }
         }while(a != 0);
     }
+}
+
 }
 
